7b52e72b386f8efbb35aa067debeca213bd8c637
kloepoitrot <KloePoitrot@hotmail.com>
2023-11-07 11:12:44 +0100

    Fix timer reset
    Timer doesn't reset at refresh anymore

--- js/script.js
+++ js/script.js
@@ -3,12 +3,15 @@ function twoDigits(num, totalLength) {
     return String(num).padStart(totalLength, '0');
 }
 
-// Check if cookie exist/If the website has been visited
-
+// If the website has been visited
+if(localStorage.getItem('timeLeft') === null){
+    localStorage.setItem('timeLeft', '14*24*60*60*1000')
+}
 
 // Initiate Countown
 let date = new Date();
-date.setTime(date.getTime()+(14*24*60*60*1000));
+let getLocal = localStorage.getItem('timeLeft')
+date.setTime(date.getTime()+(eval(getLocal)));
 
 let initnow = new Date().getTime()
 let initdistance = date - initnow
@@ -28,8 +31,6 @@ document.querySelector("#seconds h2").innerHTML = twoDigits(initseconds, 2)
 function timer() {
     let now = new Date().getTime()
     let distance = date - now
-    
-    document.cookie = "name=countdown; expires=" + date + "; path=/"
 
     let days = Math.floor(distance / (1000 * 60 * 60 * 24))
     let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
@@ -55,6 +56,8 @@ function timer() {
         }, 900)
     })
 
+    updateLocal(days, hours, minutes, seconds)
+
     if(days <= 0){
         document.querySelector("#days h2").innerHTML = twoDigits(0, 2)
     }
@@ -75,3 +78,13 @@ function timer() {
 }
 
 setInterval(() => timer(), 1000)
+
+
+// Update LocalHost
+function updateLocal(d, h, m, s) {
+    s = s * 1000
+    m = m * 1000 * 60
+    h = h * 1000 * 60 * 60
+    d = d * 1000 * 60 * 60 * 24
+    localStorage.setItem('timeLeft', d + h + m + s)
+}
